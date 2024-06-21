936b5de34f7f665d3b8bdeb5f79a580d5a8ec2b5
rahul kumar <rahulkumar9142684664@gmail.com>
2024-06-22 05:19:18 +0530

    add types

---
+++ components/admin/createCourse/courseTypes.ts
@@ -0,0 +1,51 @@
+// Define the structure for a link
+export interface ILink {
+    title: string;
+    url: string;
+  }
+  
+  // Define the structure for course content
+ export  interface ICourseContentSchema {
+    contentVideo: any;
+    videoSection: string;
+    title: string;
+    description: string;
+    links: ILink[];
+    suggestion: string;
+  }
+  
+  // Define the structure for benefits and prerequisites
+  export interface IBenefit {
+    title: string;
+  }
+  export interface IPrerequisites {
+    title: string;
+  }
+  
+  // Define the structure for course information
+ export  interface ICourseInfo {
+    name: string;
+    description: string;
+    price: number;
+    estimatedPrice: number;
+    tags: string;
+    thumbnail: any;
+    level: string;
+    demoVideo: any;
+  }
+  
+  // Define the structure for the final data object
+ export  interface ICourseData {
+    name: string;
+    description: string;
+    price: number;
+    estimatedPrice: number;
+    tags: string;
+    thumbnail: any;
+    level: string;
+    demoVideo: any;
+    totalVideos: number;
+    prerequisites: IBenefit[];
+    benefits: IBenefit[];
+    courseContent: ICourseContentSchema[];
+  }
